569ca629f5c7cb8af70e507d82d9f0484c904531
Sammy0807 <sami.egwu90@gmail.com>
2019-07-21 19:10:22 -0500

    new commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb+srv://unkubrad:3Gwurube.@meanauthapp-kiirm.azure.mongodb.net/test?retryWrites=true&w=majority',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
