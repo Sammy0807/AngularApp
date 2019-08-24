38f6a401f00dcaac092c01dbf1d96684a4c75f05
Samuel Egwurube <19858047+Sammy0807@users.noreply.github.com>
2019-08-23 20:10:58 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb+srv://unkubrad:3Gwurube.@meanauthapp-kiirm.azure.mongodb.net/test?retryWrites=true&w=majority',   //prod
+  database: 'mongodb+srv://unkubrad:3Gwurube.@cluster0-kiirm.gcp.mongodb.net/test?retryWrites=true&w=majority',   //prod
+  
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
