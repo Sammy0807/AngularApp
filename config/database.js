b86115ec96dd788e43b2f6a2357835ef80185662
Sammy0807 <sami.egwu90@gmail.com>
2019-09-12 11:39:03 -0500

    the commits

--- config/database.js
+++ config/database.js
@@ -1,11 +1,8 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-<<<<<<< HEAD
   database: process.env.CONNECTIONSTRING, //prod
-=======
   database: newFunction(), //prod
-  //database: process.env.CONNECTIONSTRING,   
->>>>>>> 13798b7f6d9f7521d098b96aea0f681ba07db721
+  //database: process.env.CONNECTIONSTRING,
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: "yoursecret"
 };
